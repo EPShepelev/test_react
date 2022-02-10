aebbe70c316b9e7e34a0422cc6cdd602c3680ee3
Шепелев Евгений <shepelev.e@ad1team.ru>
2022-02-10 14:49:11 +0300

    delet widget creating func, redy to watch in dev mode

--- weather/src/index.tsx
+++ weather/src/index.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-declare const window: any;
-window.WeatherWidget = (elementId: string) => {
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById(elementId)
+  document.getElementById("root")
   );
-  return this
-}
